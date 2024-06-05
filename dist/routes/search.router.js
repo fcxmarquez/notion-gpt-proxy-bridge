c137f024f59a1dab88523dd714af9fe58d6153d3
fcxmarquez <francisco.marquez.solt@gmail.com>
2024-06-04 18:50:32 -0600

    build: updated vercel config

--- dist/routes/search.router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.searchRouter = void 0;
-const express_1 = require("express");
-const search_controller_1 = require("../controllers/search.controller");
-const router = (0, express_1.Router)();
-router.get("/", search_controller_1.getSearch);
-exports.searchRouter = router;
-//# sourceMappingURL=search.router.js.map
