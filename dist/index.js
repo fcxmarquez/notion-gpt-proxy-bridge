976fa4afb71dacc88ed6e93380267e301cbebf5d
fcxmarquez <francisco.marquez.solt@gmail.com>
2024-06-23 10:04:28 -0600

    feat: added notion proxy

--- dist/index.js
+++ dist/index.js
@@ -7,11 +7,32 @@ const express_1 = __importDefault(require("express"));
 const config_1 = require("./config/config");
 const middleware_1 = require("./middleware");
 const routes_1 = require("./routes");
+const axios_1 = __importDefault(require("axios"));
 const app = (0, express_1.default)();
 const PORT = config_1.config.port || 3000;
 app.use(express_1.default.json());
 app.use(middleware_1.authenticateRequest);
 (0, routes_1.routerApi)(app);
+const notionProxy = async (req, res, next) => {
+    try {
+        const authorization = req.headers.authorization || "";
+        const response = await (0, axios_1.default)({
+            method: req.method,
+            url: `https://api.notion.com/v1${req.path}`,
+            headers: {
+                Authorization: authorization,
+                "Notion-Version": "2022-06-28",
+                "Content-Type": "application/json",
+            },
+            data: req.body,
+        });
+        res.status(response.status).json(response.data);
+    }
+    catch (error) {
+        next(error);
+    }
+};
+app.use("/notion", notionProxy);
 app.use(middleware_1.handleError);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import { config } from "@/config/config";
 import { authenticateRequest, handleError } from "@/middleware";
 import { routerApi } from "@/routes";
+import axios from "axios";
 
 const app = express();
 const PORT = config.port || 3000;
@@ -10,6 +11,27 @@ app.use(express.json());
 app.use(authenticateRequest);
 
 routerApi(app);
+
+const notionProxy = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const authorization = req.headers.authorization || "";
+    const response = await axios({
+      method: req.method,
+      url: `https://api.notion.com/v1${req.path}`,
+      headers: {
+        Authorization: authorization,
+        "Notion-Version": "2022-06-28",
+        "Content-Type": "application/json",
+      },
+      data: req.body,
+    });
+    res.status(response.status).json(response.data);
+  } catch (error) {
+    next(error);
+  }
+};
+
+app.use("/notion", notionProxy);
 
 app.use(handleError);
 
